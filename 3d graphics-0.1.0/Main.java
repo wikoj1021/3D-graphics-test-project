f83edc45305f59637ce13fdf134bfb36de38caf8
wikoj1021 <wikoj1021@gmail.com>
2017-12-11 17:35:08 +0100

    modified main

--- 3d graphics-0.1.0/Main.java
+++ 3d graphics-0.1.0/Main.java
@@ -10,25 +10,7 @@ import window.Window;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-		Date date = new Date();
-		
-		final String FILE_PATH  = "log_"+dateFormat.format(date)+".txt";
-		
-		System.out.println(FILE_PATH);
-		File log = new File(FILE_PATH);
-		FileWriter writeLog;
-		try{
-			log.createNewFile();
-			writeLog = new FileWriter(log);
-			writeLog.write("test");
-			writeLog.close();
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
+	
 		EventGet window = new EventGet();
 		
 	}
